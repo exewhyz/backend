d20e633ee963ab0b73ff7b0daf2618e16d74733d
ExeWhyZ <araj52444@gmail.com>
2022-11-16 15:46:27 +0530

    backend complete

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const connectToMongo = require('./db');
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req,res)=>{
