d31e36cb14cca16688e8c56cae5693c588f1106d
ExeWhyZ <araj52444@gmail.com>
2022-11-25 23:50:47 +0530

    test

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ var fetchuser = require('../middleware/fetchuser');
 const JWT_SECRET =process.env.JWT_SECRET;
 
 //Route 1 : Create user using : POST "/api/auth/createuser"
-router.post('/createuser', [
+router.get('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 8 characters').isLength({ min: 8 })
